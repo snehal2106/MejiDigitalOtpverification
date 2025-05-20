c4a70ac5f182ccc2569bd26d8493f2cd4f8d90b2
Snehal More <snehal2106@users.noreply.github.com>
2025-05-20 23:20:14 +0530

    storage

---
+++ storage/framework/views/d48ef54c714cf36c5a7bce3a141526f9.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Meji_Digital\laravel10-otp\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
